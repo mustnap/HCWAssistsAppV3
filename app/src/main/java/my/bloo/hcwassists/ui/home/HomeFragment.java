777f8262ffc892b2d11a6df51729e13c1fb66e88
Munsif-XPS15-Ubuntu <mohdmunsif@gmail.com>
2022-12-22 16:14:03 +0800

    Add working

--- app/src/main/java/my/bloo/hcwassists/ui/home/HomeFragment.java
+++ app/src/main/java/my/bloo/hcwassists/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package my.bloo.hcwassists.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,10 +10,19 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import my.bloo.hcwassists.AddActivity;
+import my.bloo.hcwassists.MainActivity;
+import my.bloo.hcwassists.R;
 import my.bloo.hcwassists.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
+
+    RecyclerView recyclerView;
+    FloatingActionButton add_button;
 
     private FragmentHomeBinding binding;
 
@@ -23,6 +33,19 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        recyclerView = root.findViewById(R.id.recyclerView);
+        add_button = root.findViewById(R.id.add_button);
+
+       add_button.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                     Intent intent = new Intent(getActivity(), AddActivity.class);
+                     startActivity(intent);
+
+             }
+         });
+
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
