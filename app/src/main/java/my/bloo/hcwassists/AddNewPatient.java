6608ff5625bd25a749cf851d01c0fadc91ba9b84
Munsif-XPS15-Ubuntu <mohdmunsif@gmail.com>
2023-01-05 01:57:03 +0800

    adding add patient

---
+++ app/src/main/java/my/bloo/hcwassists/AddNewPatient.java
@@ -0,0 +1,14 @@
+package my.bloo.hcwassists;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNewPatient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_patient);
+    }
+}
--- app/src/main/java/my/bloo/hcwassists/ui/home/HomeFragment.java
+++ app/src/main/java/my/bloo/hcwassists/ui/home/HomeFragment.java
@@ -15,6 +15,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import my.bloo.hcwassists.AddActivity;
+import my.bloo.hcwassists.AddNewPatient;
 import my.bloo.hcwassists.MainActivity;
 import my.bloo.hcwassists.R;
 import my.bloo.hcwassists.databinding.FragmentHomeBinding;
@@ -40,7 +41,7 @@ public class HomeFragment extends Fragment {
        add_button.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View view) {
-                     Intent intent = new Intent(getActivity(), AddActivity.class);
+                     Intent intent = new Intent(getActivity(), AddNewPatient.class);
                      startActivity(intent);
 
              }
