8674415098ddf73f1797583743704bc05efb15a9
Munsif-XPS15-Ubuntu <mohdmunsif@gmail.com>
2023-01-06 00:35:41 +0800

    need to inflate row next

--- app/src/main/java/my/bloo/hcwassists/CustomAdapter.java
+++ app/src/main/java/my/bloo/hcwassists/CustomAdapter.java
@@ -1,5 +1,8 @@
 package my.bloo.hcwassists;
 
+import static java.lang.Math.abs;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -16,22 +19,29 @@ import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
 
     private Context context;
     private Activity activity;
-    private ArrayList book_id, book_title, book_author, book_pages;
+    private ArrayList patient_id, patient_name, patient_age, patient_dob;
 
-    public CustomAdapter(Activity activity, Context context, ArrayList book_id, ArrayList book_title, ArrayList book_author,
-                  ArrayList book_pages){
+    CustomAdapter(Activity activity, Context context, ArrayList book_id, ArrayList patient_name, ArrayList patient_age,
+                  ArrayList patient_dob){
         this.activity = activity;
         this.context = context;
-        this.book_id = book_id;
-        this.book_title = book_title;
-        this.book_author = book_author;
-        this.book_pages = book_pages;
+        this.patient_id = book_id;
+        this.patient_name = patient_name;
+        this.patient_age = patient_age;
+        this.patient_dob = patient_dob;
     }
 
     @NonNull
@@ -44,49 +54,88 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
-    public void onBindViewHolder(@NonNull final MyViewHolder holder, final int position) {
-        holder.book_id_txt.setText(String.valueOf(book_id.get(position)));
-        holder.book_title_txt.setText(String.valueOf(book_title.get(position)));
-        holder.book_author_txt.setText(String.valueOf(book_author.get(position)));
-        holder.book_pages_txt.setText(String.valueOf(book_pages.get(position)));
-        //Recyclerview onClickListener
-//        holder.mainLayout.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(context, UpdateActivity.class);
-//                intent.putExtra("id", String.valueOf(book_id.get(position)));
-//                intent.putExtra("title", String.valueOf(book_title.get(position)));
-//                intent.putExtra("author", String.valueOf(book_author.get(position)));
-//                intent.putExtra("pages", String.valueOf(book_pages.get(position)));
-//                activity.startActivityForResult(intent, 1);
-//            }
-//        });
+    public void onBindViewHolder(@NonNull final MyViewHolder holder, @SuppressLint("RecyclerView") final int position) {
+        holder.patient_id_txt.setText(String.valueOf(patient_id.get(position)));
+        holder.patient_name_txt.setText(String.valueOf(patient_name.get(position)));
+        holder.patient_age_txt.setText(String.valueOf(patient_age.get(position)));
+        holder.patient_dob_txt.setText(String.valueOf(patient_dob.get(position)));
+
+        holder.mainLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent;
+                int thisPatientAge = Integer.valueOf(  getAgeInMonths(String.valueOf(patient_dob.get(position))) );
+//                if(  thisPatientAge >= 0 && thisPatientAge < 12 ) {
+//                    intent = new Intent(context, Form0_11months.class);
+//                } else if ( thisPatientAge >= 12 && thisPatientAge < 23  ) {
+//                    intent = new Intent(context, Form12_23months.class);
+//                } else if ( thisPatientAge >= 24 && thisPatientAge < 60  ) {
+//                    intent = new Intent(context, Form24_59months.class);
+//                } else if ( thisPatientAge >= 60 && thisPatientAge < 144  ) {
+//                    intent = new Intent(context, Form60_143months.class);
+//                } else {
+//                    intent = new Intent(context, Form144above_months.class);
+//                }
+
+
+                intent = new Intent(context, MainActivity.class);
+
+                intent.putExtra("id", String.valueOf(patient_id.get(position)));
+                intent.putExtra("name", String.valueOf(patient_name.get(position)));
+                intent.putExtra("age", String.valueOf(patient_age.get(position)));
+                intent.putExtra("dob", String.valueOf(patient_dob.get(position)));
+                activity.startActivityForResult(intent, 1);
+            }
+        });
 
 
     }
 
     @Override
     public int getItemCount() {
-        return book_id.size();
+        return patient_id.size();
     }
 
     class MyViewHolder extends RecyclerView.ViewHolder {
 
-        TextView book_id_txt, book_title_txt, book_author_txt, book_pages_txt;
+        TextView patient_id_txt, patient_name_txt, patient_age_txt, patient_dob_txt;
         LinearLayout mainLayout;
 
         MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            book_id_txt = itemView.findViewById(R.id.book_id_txt);
-            book_title_txt = itemView.findViewById(R.id.book_title_txt);
-            book_author_txt = itemView.findViewById(R.id.book_author_txt);
-            book_pages_txt = itemView.findViewById(R.id.book_pages_txt);
+            patient_id_txt = itemView.findViewById(R.id.patient_id_txt);
+            patient_name_txt = itemView.findViewById(R.id.patient_name_txt);
+            patient_age_txt = itemView.findViewById(R.id.patient_age_txt);
+            patient_dob_txt = itemView.findViewById(R.id.patient_dob_txt);
             mainLayout = itemView.findViewById(R.id.mainLayout);
             //Animate Recyclerview
-//            Animation translate_anim = AnimationUtils.loadAnimation(context, R.anim.translate_anim);
-//            mainLayout.setAnimation(translate_anim);
+            Animation translate_anim = AnimationUtils.loadAnimation(context, R.anim.translate_anim);
+            mainLayout.setAnimation(translate_anim);
         }
 
+    }
+
+    int getAgeInMonths(String dob) {
+        int ageInMonths = 0;
+        Date dateOfBirth;
+        try {
+            dateOfBirth=new SimpleDateFormat("dd-MMM-yyyy").parse(String.valueOf(dob));
+        } catch (ParseException e) {
+            e.printStackTrace();
+            dateOfBirth = new Date();
+        }
+        Date currentDate = new Date();
+        LocalDate ldCurrentDate = currentDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        LocalDate ldDateOfBirth = dateOfBirth.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+
+        long monthsBetween = ChronoUnit.MONTHS.between(
+                YearMonth.from(ldCurrentDate),
+                YearMonth.from(ldDateOfBirth)
+        );
+
+        ageInMonths = (int) monthsBetween;
+
+        return abs(ageInMonths);
     }
 
 }
