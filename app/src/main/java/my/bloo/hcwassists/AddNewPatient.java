5883ab410cb5e401615cdbda46bf7fcf33ef8795
Munsif-XPS15-Ubuntu <mohdmunsif@gmail.com>
2023-01-05 23:34:22 +0800

    adding patient works

--- app/src/main/java/my/bloo/hcwassists/AddNewPatient.java
+++ app/src/main/java/my/bloo/hcwassists/AddNewPatient.java
@@ -1,14 +1,129 @@
 package my.bloo.hcwassists;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.icu.text.SimpleDateFormat;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.datepicker.CalendarConstraints;
+import com.google.android.material.datepicker.CompositeDateValidator;
+import com.google.android.material.datepicker.DateValidatorPointBackward;
+import com.google.android.material.datepicker.DateValidatorPointForward;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 public class AddNewPatient extends AppCompatActivity {
+
+    private Button mPickDateButton;
+    EditText name_input, age_input, dob_input;
+    Button add_button;
+
+    private TextInputEditText txtEditPatientDoB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_patient);
+
+        // now register the text view and the button with
+        // their appropriate IDs
+        mPickDateButton = findViewById(R.id.pick_date_button);
+        txtEditPatientDoB = findViewById(R.id.text_input_dob);
+
+        // now create instance of the material date picker
+        // builder make sure to add the "datePicker" which
+        // is normal material date picker which is the first
+        // type of the date picker in material design date
+        // picker
+        MaterialDatePicker.Builder materialDateBuilder = MaterialDatePicker.Builder.datePicker();
+
+        // now define the properties of the
+        // materialDateBuilder that is title text as SELECT A DATE
+        materialDateBuilder.setTitleText("SELECT A DATE");
+
+        CalendarConstraints.Builder constraintsBuilderRange = new CalendarConstraints.Builder();
+
+//....define min and max for example with LocalDateTime and ZonedDateTime or Calendar
+        Calendar rightNow = Calendar.getInstance();
+        Calendar beforeCutoff = Calendar.getInstance();
+        beforeCutoff.add(Calendar.YEAR, -17);
+
+        CalendarConstraints.DateValidator dateValidatorMin = DateValidatorPointForward.from(beforeCutoff.getTimeInMillis());
+        CalendarConstraints.DateValidator dateValidatorMax = DateValidatorPointBackward.before(rightNow.getTimeInMillis());
+
+        ArrayList<CalendarConstraints.DateValidator> listValidators =
+                new ArrayList<CalendarConstraints.DateValidator>();
+        listValidators.add(dateValidatorMin);
+        listValidators.add(dateValidatorMax);
+        CalendarConstraints.DateValidator validators = CompositeDateValidator.allOf(listValidators);
+        constraintsBuilderRange.setValidator(validators);
+
+        // now create the instance of the material date
+        // picker
+
+        materialDateBuilder.setCalendarConstraints(constraintsBuilderRange.build());
+        final MaterialDatePicker materialDatePicker = materialDateBuilder.build();
+
+        // handle select date button which opens the
+        // material design date picker
+        mPickDateButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // getSupportFragmentManager() to
+                        // interact with the fragments
+                        // associated with the material design
+                        // date picker tag is to get any error
+                        // in logcat
+                        materialDatePicker.show(getSupportFragmentManager(), "MATERIAL_DATE_PICKER");
+                    }
+                });
+
+        // now handle the positive button click from the
+        // material design date picker
+        materialDatePicker.addOnPositiveButtonClickListener(
+                new MaterialPickerOnPositiveButtonClickListener() {
+                    @SuppressLint("SetTextI18n")
+                    @Override
+                    public void onPositiveButtonClick(Object selection) {
+
+                        // if the user clicks on the positive
+                        // button that is ok button update the
+                        // selected date
+                        String dateString = DateFormat.format("dd-MMM-yyyy", new Date((long) selection)).toString();
+                        txtEditPatientDoB.setText(dateString);
+                        // in the above statement, getHeaderText
+                        // is the selected date preview from the
+                        // dialog
+
+                    }
+                });
+
+        name_input = findViewById(R.id.text_input_patient_name);
+        dob_input = findViewById(R.id.text_input_dob);
+
+        add_button = findViewById(R.id.add_button);
+
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyDatabaseHelper myDB = new MyDatabaseHelper(AddNewPatient.this);
+                myDB.addPatient(name_input.getText().toString().trim(),
+                        dob_input.getText().toString());
+
+            }
+        });
     }
 }
--- app/src/main/java/my/bloo/hcwassists/MyDatabaseHelper.java
+++ app/src/main/java/my/bloo/hcwassists/MyDatabaseHelper.java
@@ -1,24 +1,37 @@
 package my.bloo.hcwassists;
 
+import static java.lang.Math.abs;
+
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
-public class MyDatabaseHelper extends SQLiteOpenHelper {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+class MyDatabaseHelper extends SQLiteOpenHelper {
 
     private Context context;
-    private static final String DATABASE_NAME = "HCWAssist.db";
+    private static final String DATABASE_NAME = "Patients.db";
     private static final int DATABASE_VERSION = 1;
 
     private static final String TABLE_NAME = "patient_list";
     private static final String COLUMN_ID = "_id";
     private static final String COLUMN_NAME = "name";
-    private static final String COLUMN_BIRTHDATE = "dob";
     private static final String COLUMN_AGE = "age";
+    private static final String COLUMN_DOB = "dob";
 
-    public MyDatabaseHelper(@Nullable Context context ) {
+    MyDatabaseHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
         this.context = context;
     }
@@ -27,17 +40,95 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         String query = "CREATE TABLE " + TABLE_NAME +
                 " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                COLUMN_ID + " TEXT, " +
                 COLUMN_NAME + " TEXT, " +
-                COLUMN_BIRTHDATE + " DATE, " +
-                COLUMN_AGE + " INTEGER);";
+                COLUMN_AGE + " INTEGER, " +
+                COLUMN_DOB + " TEXT);";
         db.execSQL(query);
-
     }
-
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
         onCreate(db);
     }
+
+    void addPatient(String name, String dob){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_NAME, name);
+        cv.put(COLUMN_AGE, getAgeInMonths(dob));
+        cv.put(COLUMN_DOB, dob);
+        long result = db.insert(TABLE_NAME,null, cv);
+        if(result == -1){
+            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(context, "Added Successfully!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    Cursor readAllData(){
+        String query = "SELECT * FROM " + TABLE_NAME;
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = null;
+        if(db != null){
+            cursor = db.rawQuery(query, null);
+        }
+        return cursor;
+    }
+
+    void updateData(String row_id, String name, int age, String dob){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(COLUMN_NAME, name);
+        cv.put(COLUMN_DOB, dob);
+        cv.put(COLUMN_AGE, getAgeInMonths(dob));
+
+        long result = db.update(TABLE_NAME, cv, "_id=?", new String[]{row_id});
+        if(result == -1){
+            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(context, "Updated Successfully!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    void deleteOneRow(String row_id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        long result = db.delete(TABLE_NAME, "_id=?", new String[]{row_id});
+        if(result == -1){
+            Toast.makeText(context, "Failed to Delete.", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(context, "Successfully Deleted.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    void deleteAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("DELETE FROM " + TABLE_NAME);
+    }
+
+    int getAgeInMonths(String dob) {
+        int ageInMonths = 0;
+        Date dateOfBirth;
+        try {
+            dateOfBirth=new SimpleDateFormat("dd-MMM-yyyy").parse(String.valueOf(dob));
+        } catch (ParseException e) {
+            e.printStackTrace();
+            dateOfBirth = new Date();
+        }
+        Date currentDate = new Date();
+        LocalDate ldCurrentDate = currentDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        LocalDate ldDateOfBirth = dateOfBirth.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+
+        long monthsBetween = ChronoUnit.MONTHS.between(
+                YearMonth.from(ldCurrentDate),
+                YearMonth.from(ldDateOfBirth)
+        );
+
+        ageInMonths = (int) monthsBetween;
+
+        return abs(ageInMonths);
+    }
+
 }
